bc9467139fde59159e8fb91ef3a672fe12524086
daniel_marinho <daniel_marinho@id.uff.br>
2016-01-20 13:11:51 -0200

    -Ta andando

--- src/coddecparidadepar/Codificador.java
+++ src/coddecparidadepar/Codificador.java
@@ -18,6 +18,48 @@ import java.util.BitSet;
  */
 public class Codificador {
 
+    public static int[][] bitSetToIntMatrix(BitSet bs) {
+        return null;
+    }
+
+    public static BitSet calculaVerificadorVertical(BitSet bs) {
+        int paridade;
+        BitSet verificadorVertical = new BitSet(8);
+        for (int i = 0; i < 64; i = i + 8) {
+            paridade = 0;
+            for (int j = 0; j < 8; j++) {
+                paridade = paridade + ((bs.get(i + j)) ? 1 : 0);
+            }
+            if (paridade % 2 == 0) {
+                verificadorVertical.set(i, false);
+            } else {
+                verificadorVertical.set(i, true);
+            }
+        }
+
+        return verificadorVertical;
+    }
+
+    public static BitSet calculaVerificadorHorizontal(BitSet bs) {
+        int paridade;
+        BitSet verificadorHorizontal = new BitSet(8);
+        for (int i = 0; i < 8; i++) {
+            paridade = 0;
+            for (int j = 0; j < 64; j = j + 8) {
+                paridade = paridade + ((bs.get(i + j)) ? 1 : 0);
+                //System.out.println(bs.get(i+j));
+            }
+            if (paridade % 2 == 0) {
+                verificadorHorizontal.set(i, false);
+            } else {
+                verificadorHorizontal.set(i, true);
+            }
+            // System.out.println(paridade);
+        }
+        //System.out.println(verificadorHorizontal);
+        return verificadorHorizontal;
+    }
+
     public static void main(String[] args) throws FileNotFoundException, IOException {
         String nomeArq = "Untitled.png";
         File arquivo = new File(nomeArq);
@@ -25,28 +67,46 @@ public class Codificador {
         FileInputStream fis = new FileInputStream(arquivo);
         FileOutputStream fos = new FileOutputStream(saida);
 
-        byte[] aux = new byte[8];
-        byte[] aux2 = new byte[8];
-        int bytesRead = fis.read(aux, 0, 8); //serão necessarios 2 bytes para o calculo
+        byte[] bytesIn = new byte[8];
+        byte[] bytesOut = new byte[8];
+        int bytesRead = fis.read(bytesIn, 0, 8); //serão necessarios 2 bytes para o calculo
 
-        BitSet  matrizBits = new BitSet(64);
+        BitSet matrizBits = new BitSet(64);
         BitSet verificadorVerticalBits = new BitSet(8);
         BitSet verificadorHorizontalBits = new BitSet(8);
-        
-        int numBitSet = 0;
-        int i = 0;
+
         while (bytesRead != -1) {
+
+            matrizBits = BitSet.valueOf(bytesIn);
+            //System.out.println("Matriz Original");
             
-            matrizBits = BitSet.valueOf(aux);
+            
+            System.out.println("");
+            verificadorVerticalBits = calculaVerificadorVertical(matrizBits);
+            System.out.println("Vetor Vertical");
+            System.out.print(verificadorVerticalBits);
 
-            aux2 = matrizBits.toByteArray();
-            fos.write(aux2);
-            while(i<64){
-                
-            }
-            bytesRead = fis.read(aux, 0, 8);
+            verificadorHorizontalBits = calculaVerificadorHorizontal(matrizBits);
+            System.out.println("Vetor Horizontal");
+            System.out.print(verificadorHorizontalBits);
+            
+            bytesOut = verificadorHorizontalBits.toByteArray();
+            fos.write(bytesOut);
+            bytesOut = verificadorVerticalBits.toByteArray();
+            fos.write(bytesOut);
+            bytesOut = matrizBits.toByteArray();
+            fos.write(bytesOut);
+//            for (int j = 0; j < 64; j++) {
+//                //System.out.print(matrizBits.get(j) + " ");
+//                if (j % 8 == 0) {
+//                    //System.out.print("\n");
+//                }
+//            }
+            
+
+            System.out.println("");
+            bytesRead = fis.read(bytesIn, 0, 8);
         }
-
 
         fos.close();
         fis.close();
